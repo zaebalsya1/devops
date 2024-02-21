47e8aaa89918e0370518cd93f8a6ab159f97b5b6
Itachi Uchiha <zaebalsya13@gmail.com>
2024-02-21 14:30:25 +0200

    Ваши изменения

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+module.exports = {
+  preset: 'jest-puppeteer',
+  testMatch: ['**/test/**/*.test.js'],
+  // Другие настройки Jest...
+};
---
+++ test/buttonColor.test.js
@@ -0,0 +1,17 @@
+describe('Проверка цветов кнопок', () => {
+  beforeAll(async () => {
+    await page.goto('file:///C:/Users/Albert/Documents/DevOps_project/index.html');
+
+
+  });
+
+  it('Красная кнопка имеет правильный цвет', async () => {
+    const color = await page.$eval('#redButton', e => getComputedStyle(e).backgroundColor);
+    expect(color).toBe('rgb(255, 0, 0)'); // или 'red', в зависимости от браузера
+  });
+
+  it('Синяя кнопка имеет правильный цвет', async () => {
+    const color = await page.$eval('#blueButton', e => getComputedStyle(e).backgroundColor);
+    expect(color).toBe('rgb(0, 0, 255)'); // или 'blue', в зависимости от браузера
+  });
+});
